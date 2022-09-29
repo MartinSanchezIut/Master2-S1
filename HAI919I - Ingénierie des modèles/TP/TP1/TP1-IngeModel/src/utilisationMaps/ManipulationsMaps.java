7f90ca07e7365d69f21832d4b08ef1dc956907f8
MartinSanchezIut <martin.sanchez.iut@gmail.com>
2022-09-29 14:47:07 +0200

    tp ingé modele fini

--- HAI919I - Ingénierie des modèles/TP/TP1/TP1-IngeModel/src/utilisationMaps/ManipulationsMaps.java
+++ HAI919I - Ingénierie des modèles/TP/TP1/TP1-IngeModel/src/utilisationMaps/ManipulationsMaps.java
@@ -1,7 +1,16 @@
 package utilisationMaps;
 
 import Maps.MapsPackage;
+import Maps.Pedestrian;
+import Maps.Road;
+import Maps.Street;
+import Maps.impl.MapsFactoryImpl;
+import Maps.Garden;
 import Maps.Map;
+import Maps.MapsFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.emf.*;
 import org.eclipse.emf.common.util.*;
@@ -26,7 +35,89 @@ public class ManipulationsMaps {
 		//Instruction récupérant le modèle sous forme d'arbre à partir de la classe racine "Map"
 		maMap = (Map) resource.getContents().get(0);
 		System.out.println(maMap.getName());	// affichage du nom de la carte.
+		
+		
+		for(Street s : returnListOfStreets(maMap)) {
+			System.out.println(s.getName());
+		}
+		for(Pedestrian s : returnListOfPedestrian(maMap)) {
+			System.out.println(s.getName());
+		}		
+		
+		for(Street s : returnStreetsNextTo(maMap, "RueDeLaSoif")) {
+			System.out.println(s.getName());
+		}
+		
+		Garden test = createGarden(maMap, "COUCOU", new ArrayList<Road>()) ;
+		System.out.println(test.getName());
+		
 	}
+	
+	public static Garden createGarden(Map map, String name, List<Road> roads) {
+		Garden g = MapsFactory.eINSTANCE.createGarden() ;
+		g.setName(name);
+		g.getBorderedfly().addAll(roads) ;
+		map.getSpaces().add(g) ;
+		
+		sauverModele("test.xmi", map);
+		return g;	
+	}
+	
+	/**
+	 * Return list of streets next to the street name given
+	 * @param roadName
+	 * @return List<Street>
+	 */
+	public static List<Street> returnStreetsNextTo(Map thatMap, String roadName) {
+		ArrayList<Street> ret = new ArrayList<Street>();
+		
+		for(Road r : thatMap.getRoads()) {
+			if (r.getName().equalsIgnoreCase(roadName)) {
+				for(Road s : r.getMeet()) {
+					if (s instanceof Street) {
+						ret.add((Street) s) ;
+					}
+				}
+			}
+		}	
+		return ret;
+	}
+	
+	
+	
+	/**
+	 * Return list of "Pedestrian" of "thatMap"
+	 * @param thatMap
+	 * @return List<Pedestrian>
+	 */
+	public static List<Pedestrian> returnListOfPedestrian(Map thatMap) {
+		ArrayList<Pedestrian> ret = new ArrayList<Pedestrian>();
+		for(Road r : thatMap.getRoads()) {
+			if (r instanceof Pedestrian) {
+				ret.add((Pedestrian) r) ;
+			}
+		}
+		return ret;
+	}
+		
+	
+	/**
+	 * Retourne la liste des "Street" de "thatMap"
+	 * @param thatMap 
+	 * @return List<Street>
+	 */
+	public static List<Street>  returnListOfStreets(Map thatMap) {
+		ArrayList<Street> ret = new ArrayList<Street>();
+		
+		for(Road r : thatMap.getRoads()) {
+			if (r instanceof Street) {
+				ret.add((Street) r) ;
+			}
+		}
+		return ret ;
+	}
+	
+	
 	
 	public static Resource chargerModele(String uri, EPackage pack) {
 		   Resource resource = null;
