e74266c4c1d785c1bfcc82d15ff76a74322eadb4
MartinSanchezIut <martin.sanchez.iut@gmail.com>
2022-11-25 10:17:23 +0100

    avancement evol refactorign

---
+++ HAI913I - Evolution et restructuration des logiciels/TPs/java tp5/src/beforeRefactor/EncapsulateField.java
@@ -0,0 +1,14 @@
+package beforeRefactor;
+
+/* 
+ * Author : SANCHEZ Martin
+ */
+
+public class EncapsulateField {
+
+	public int toto;
+	
+	public EncapsulateField(int t) {
+		this.toto = t;
+	}
+}
